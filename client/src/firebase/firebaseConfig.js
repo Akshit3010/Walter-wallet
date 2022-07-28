92baa432a151c55e7dda5519e7eea255185f9f39
Tapish sharma <tapishsharma1234@gmail.com>
2022-07-28 21:32:20 +0530

    firebase config complete

--- client/src/firebase/firebaseConfig.js
+++ client/src/firebase/firebaseConfig.js
@@ -1,21 +1,22 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBKq3A5ed4oEB1cyMXhFuH069O8liNTuck",
-  authDomain: "walter-wallet.firebaseapp.com",
-  projectId: "walter-wallet",
-  storageBucket: "walter-wallet.appspot.com",
-  messagingSenderId: "567594445317",
-  appId: "1:567594445317:web:3b238ac4188d766e424a6c",
-  measurementId: "G-WBZ9524W01"
-};
+// const app = initializeApp({
+//   apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
+//   authDomain: ` ${process.env.REACT_APP_FIREBASE_AUTH_DOMAIN}`,
+//   projectId: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}`,
+//   storageBucket: `${process.env.REACT_APP_FIREBASE_STORAGE_BUCKET}`,
+//   messagingSenderId: `${process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID}`,
+//   appId: `${process.env.REACT_APP_FIREBASE_APP_ID}`,
+// });
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app = initializeApp({
+  apiKey: `AIzaSyBDkj7pH5EVNpOfefGGjHUkkpI0lpTt8Vs`,
+  authDomain: `walter-wallet-936dc.firebaseapp.com`,
+  projectId: `walter-wallet-936dc`,
+  storageBucket: `walter-wallet-936dc.appspot.com`,
+  messagingSenderId: `784025454259`,
+  appId: `1:784025454259:web:c7c86fd6e8ac86b6ebd2c6`,
+});
+
+const db = getAuth(app);
